8536544055e1fa8d833af7f4528280a43e10fe51
BraidynDenzer <braidyn.denzer@umconnect.umt.edu>
2025-03-31 11:48:28 -0600

    main

---
+++ Week 9/sketch.js
@@ -0,0 +1,30 @@
+    function setup()
+    {
+        createCanvas(500,500);
+    }
+function draw()
+{
+    background(220);
+      textSize(32);
+  text('My portrait', 170, 50);
+  
+  textSize(10);
+  text('Braidyn Denzer', 420, 490);
+    
+  rect(245,250,10,10);
+    rect(225,400,10,100); 
+    rect(265,400,10,100);
+    rect(200,259,10,130);
+    rect(290,260,10,130);
+  
+  circle(250,200,100);
+  
+  triangle(300, 259, 200, 259, 250,390);
+  triangle(250, 370, 225, 400, 275, 400);
+  
+ line(220, 220, 280, 220)
+  
+    point(230, 190);
+  point(270, 190);
+  
+}
